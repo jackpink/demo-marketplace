fa8f15d82bc4eb9ae05a73a93907bdc84fac0f40
Jack-Desktop <jackpinkerton@live.com.au>
2023-04-22 12:13:16 +1000

    fixup

--- back-end/database/migrations/1682114610207_quotations.ts
+++ back-end/database/migrations/1682114610207_quotations.ts
@@ -6,7 +6,7 @@ export default class Quotations extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id');
-      table.float("price", 8, 2).Nullable();
+      table.float("price", 8, 2).notNullable();
       table.text("comments", 'longtext').notNullable();
       table.integer('for_job').notNullable().references('id').inTable('jobs');
       table.integer('for_maker').notNullable().references('id').inTable('makers');
