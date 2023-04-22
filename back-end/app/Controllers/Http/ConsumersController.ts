265653105e63833a6dab5e1acb2fa475fc434f0e
Jack-Desktop <jackpinkerton@live.com.au>
2023-04-22 11:39:06 +1000

    consumer controller added

--- back-end/app/Controllers/Http/ConsumersController.ts
+++ back-end/app/Controllers/Http/ConsumersController.ts
@@ -1,17 +1,54 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Consumer from 'App/Models/Consumer'
 
 export default class ConsumersController {
-  public async index({}: HttpContextContract) {}
+  public async index({ response }: HttpContextContract) {
+    const consumers = await Consumer.all();
+    return response.json({ consumers });
+  }
 
-  public async create({}: HttpContextContract) {}
+  public async create({}: HttpContextContract) {
+    
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request, response }: HttpContextContract) {
+    const data = request.only(['firstName', 'lastName', 'phoneNumber', 'email', 'address'])
 
-  public async show({}: HttpContextContract) {}
+    const consumer = new Consumer();
+    consumer.firstName = data.firstName;
+    consumer.lastName = data.lastName;
+    consumer.phoneNumber = data.phoneNumber;
+    consumer.email = data.email;
+    consumer.address = data. address;    
+
+    await consumer.save()
+
+    return response.json({ consumer })
+  }
+
+  public async show({response, params}: HttpContextContract) {
+    const consumer = await Consumer.findOrFail(params.id);
+
+    return response.json({ consumer });
+  }
 
   public async edit({}: HttpContextContract) {}
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, response, params}: HttpContextContract) {
+    const data = request.only(['firstName', 'lastName', 'phoneNumber', 'email', 'address'])
+    const consumer = await Consumer.findOrFail(params.id);
+    consumer.merge(data)
+  
 
-  public async destroy({}: HttpContextContract) {}
+    await consumer.save();
+
+    return response.json( { consumer });
+  }
+
+  public async destroy({ response, params }: HttpContextContract) {
+    const consumer = await Consumer.findOrFail(params.id);
+    await consumer.delete();
+
+    return response.json({ consumer });
+  }
 }
